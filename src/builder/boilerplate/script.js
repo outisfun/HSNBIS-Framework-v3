be3fba4d347bda53cd28dd9f3118e252b54d1c5f
Katerina Vaseva <katerina.vsv@gmail.com>
2019-11-22 14:46:00 +0100

    update boilerplate scripts.js

--- src/builder/boilerplate/script.js
+++ src/builder/boilerplate/script.js
@@ -8,37 +8,48 @@ var ScrollMagic = require('scrollmagic');
 var TEMPLATES = require('builder/templates.js'); // reconsider namespace?
 var PrismicToISF = require('builder/prismic-to-isf.js');
 
-var ISFChapter = require('chapter/script.js');
-var ISFNavTabs = require('tabs/script.js');
+const ISFDATA = require('builder/isf-data.js');
 
-
+var ISF = {};
+ISF.ISF_Layout_Chapter = require('chapter/script.js');
+ISF.ISF_Layout_SplitSticky = require('splitsticky/script.js');
+ISF.ISF_Element_Gallery = require('gallery/script.js');
+ISF.ISF_Nav_Tabs = require('tabs/script.js');
 
 ( function(window) {
 
   'use strict';
 
-  var constructors = {};
-
   function ISFStory (el, prismicData) {
+    this.controller = new ScrollMagic.Controller();
     this.DOM = { el: el };
     this.storyData = new PrismicToISF(prismicData).isfData;
     this.markup = this.buildMarkup();
 
     this.DOM.el.innerHTML = this.markup;
 
+    // refactor by adding a loader or making sure
+    // in another way that markup is built and calculated correctly
+    // before constructors start working ;)
     setTimeout(() => {
       this.initConstructors();
-    }, 1000);
+    }, 500);
   }
 
   ISFStory.prototype.initConstructors = function () {
-    this.controller = new ScrollMagic.Controller();
 
-    var tabs = this.DOM.el.querySelector(".js--nav_tabs");
-    var navTabs = new ISFNavTabs(tabs, this.controller);
-    _.forEach (Array.from(this.DOM.el.querySelectorAll(".js--chapter")), (el, index) => {
-      var _chapter = new ISFChapter(el, this.controller);
-      _chapter.init();
+    _.forOwn(ISFDATA, (moduleGroup, moduleType) => {
+      _.forOwn(moduleGroup, (elData, elName) => {
+        if (elData.CONSTRUCTOR !== undefined) {
+          // init elements
+          if (elData.CLASSES.EL !== undefined) {
+            _.forEach(Array.from(this.DOM.el.querySelectorAll("." + elData.CLASSES.EL)), (el, index) => {
+              var _el = new ISF[elData.CONSTRUCTOR](el, this.controller);
+              _el.init();
+            });
+          }
+        }
+      });
     });
   };
 
@@ -57,6 +68,8 @@ var ISFNavTabs = require('tabs/script.js');
     _.forOwn(this.storyData.layouts, (layout, layoutId) => {
       var layoutType = this._getType(layoutId);
       layout.layoutData.modId = layoutId;
+
+      // refactor this by making it an entry in prismic
       layout.layoutData.chapterStyle = "ellie";
       if ((TEMPLATES)[layoutType] !== undefined) {
         var layoutMarkup = TEMPLATES[layoutType](layout.layoutData);
@@ -84,11 +97,14 @@ var ISFNavTabs = require('tabs/script.js');
     return markup;
   };
 
+  // refactor this by considering
+  // other types of layouts
   ISFStory.prototype._getType = function (id) {
     return _.includes(id, 'chapter') ? 'chapter' : 'simple'; // export to a function
   };
 
   // load data from prismic
+  // refactor by loading the id dynamically or something
   Prismic.getApi("https://hsnbsttest.prismic.io/api/v2",  {}).then(function(api) {  return api.query(
     Prismic.Predicates.at('document.id', 'XdVcShEAACIA-rOn'), {} );
   }).then(function(response) {
