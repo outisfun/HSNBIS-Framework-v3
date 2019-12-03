e8fc860a4469c09b67cf154934f7691f96120bf7
Katerina Vaseva <katerina.vsv@gmail.com>
2019-12-03 16:05:45 +0100

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# HIGHSNOB INTERACTIVE STORY FRAMEWORK V3 (+ Prismic)
+# HS Interactive Story Framework V3 (+ Prismic)
 
 A framework to speed up development of Tier 3 Interactive Stories for Highsnobiety.
 
@@ -6,12 +6,13 @@ A framework to speed up development of Tier 3 Interactive Stories for Highsnobie
 ## Overview
 
 Version 3 of the framework enables editors to dynamically add, remove or edit content (copy and images) through Prismic.
-Prismic gives access to a few modules which correspond to the framework modules (layouts, elements, navigation), so that editors can build stories independently, with a visual style builder.
-It's still somewhat beta, and major differences with the previous setup will be outline below.
+It's still somewhat beta, and major differences with the previous setup, as well as current questions and developments in progress will be outlined below.
 
 Each story consists of two components:
 - An interactive story post in Prismic, which has a unique ID and provides a JSON with all the content;
-- Scripts and styles, as usual. The difference is that while in development/client preview mode, the markup is generated dynamically with the scripts. In future this will probably change, but we're still testing and working out the best approach. When the page goes live, it is important that the markup is added as the regular HTML to avoid any risks.
+- Scripts and styles, as usual.
+
+*The difference is that while in development/client preview mode, the markup is generated dynamically with the scripts, so the developer doesn't need to be involved. When the page goes live, it is important that the markup is added as the regular HTML to avoid any risks.*
 
 
 ## Getting Started
@@ -30,7 +31,9 @@ pass: (Ask me :)
 
 ## Creating a story
 
-### Create a new interactive story post in Prismic
+### Development / Client preview mode
+
+I. Create a new interactive story post in Prismic
 
 Go to https://hsnbsttest.prismic.io/documents/working/ and click on "Create New", which will take you to the visual editor.
 Add a title, press **Save** and then **Publish**.
@@ -41,11 +44,15 @@ The first part of the unique string in the end of the URL is your Prismic ID. In
 It may or may not differ from the second one, but that's what you need.
 
 
-### Workflow (current beta setup - will change)
+II. Generate scripts and styles
 
 Create a story, as usual:
 ```
 $ grunt init-story --story=YYYY-MM-projectName
 ```
 
-Create a
+Go to YYYY-MM-projectName/build/scripts.js and add the PrismicId you got in the previous step. This allows the story to pull the correct JSON from Prismic.
+```
+const prismicDocumentId = 'XeZayBEAAI8DRhu';
+```
+
--- src/builder/boilerplate/script.js
+++ src/builder/boilerplate/script.js
@@ -1,118 +1,24 @@
 
+const Prismic = require('prismic-javascript');
+const ISFStory = require('common/js/isf-story.js');
 
-var _ = require('lodash');
-var Prismic = require('prismic-javascript');
-var Handlebars = require('handlebars');
-var ScrollMagic = require('scrollmagic');
-
-var TEMPLATES = require('builder/templates.js'); // reconsider namespace?
-var PrismicToISF = require('builder/prismic-to-isf.js');
-
-const ISFDATA = require('builder/isf-data.js');
-
-var ISF = {};
-ISF.ISF_Layout_Chapter = require('chapter/script.js');
-ISF.ISF_Layout_SplitSticky = require('splitsticky/script.js');
-ISF.ISF_Element_Gallery = require('gallery/script.js');
-ISF.ISF_Nav_Tabs = require('tabs/script.js');
-
-// CHANGE THIS!!!
-var prismicDocumentId = 'XdVcShEAACIA-rOn';
+// CHANGE THOSE!
+const ISFDataConverter = require('utilities/prismic-to-isf.js');
+const prismicDocumentId = 'Xdz5cxEAAGNuHHK7';
 
 ( function(window) {
 
   'use strict';
 
-  function ISFStory (el, prismicData) {
-    this.controller = new ScrollMagic.Controller();
-    this.DOM = { el: el };
-    this.storyData = new PrismicToISF(prismicData).isfData;
-    this.markup = this.buildMarkup();
-
-    this.DOM.el.innerHTML = this.markup;
-
-    // refactor by adding a loader or making sure
-    // in another way that markup is built and calculated correctly
-    // before constructors start working ;)
-    setTimeout(() => {
-      this.initConstructors();
-    }, 500);
-  }
-
-  ISFStory.prototype.initConstructors = function () {
-
-    _.forOwn(ISFDATA, (moduleGroup, moduleType) => {
-      _.forOwn(moduleGroup, (elData, elName) => {
-        if (elData.CONSTRUCTOR !== undefined) {
-          // init elements
-          if (elData.CLASSES.EL !== undefined) {
-            _.forEach(Array.from(this.DOM.el.querySelectorAll("." + elData.CLASSES.EL)), (el, index) => {
-              var _el = new ISF[elData.CONSTRUCTOR](el, this.controller);
-              _el.init();
-            });
-          }
-        }
-      });
-    });
-  };
-
-  ISFStory.prototype.buildMarkup = function () {
-
-    var markup = '';
-
-    // build nav
-    if (this.storyData.nav !== undefined) {
-      // after we have other types of nav, change this, so that
-      // tabs is generative (it could be sth else)
-      var navMarkup = TEMPLATES.tabs(this.storyData.nav);
-      markup += navMarkup;
-    }
-
-    _.forOwn(this.storyData.layouts, (layout, layoutId) => {
-      var layoutType = this._getType(layoutId);
-      layout.layoutData.modId = layoutId;
-
-      // refactor this by making it an entry in prismic
-      layout.layoutData.chapterStyle = "ellie";
-      if ((TEMPLATES)[layoutType] !== undefined) {
-        var layoutMarkup = TEMPLATES[layoutType](layout.layoutData);
-      } else {
-        console.log('no template found for layout ' + layoutType);
-      }
-
-      // now, get elements markup
-      var layoutContentMarkup = '';
-      _.forOwn(layout.layoutContent, (element, elementId) => {
-        element.elementData.moduleId = elementId;
-        if (TEMPLATES[element.elementType] !== undefined) {
-          var elementMarkup = TEMPLATES[element.elementType](element.elementData);
-          layoutContentMarkup += elementMarkup;
-        } else {
-          console.log('no template found for element ' + element.elementType);
-        }
-
-      });
-
-      layoutMarkup = layoutMarkup.replace("<!-- content -->", layoutContentMarkup);
-      markup += layoutMarkup;
-    });
-
-    return markup;
-  };
-
-  // refactor this by considering
-  // other types of layouts
-  ISFStory.prototype._getType = function (id) {
-    return _.includes(id, 'chapter') ? 'chapter' : 'simple'; // export to a function
-  };
+  // load utility converter
+  var dataConverter = new ISFDataConverter();
 
   // load data from prismic
-  // refactor by loading the id dynamically or something
   Prismic.getApi("https://hsnbsttest.prismic.io/api/v2",  {}).then(function(api) {  return api.query(
     Prismic.Predicates.at('document.id', prismicDocumentId), {} );
   }).then(function(response) {
-    var story = new ISFStory(document.querySelector("#isf-interactive-story"), response.results[0].data);
+    var storyData = dataConverter.convert(response);
+    var story = new ISFStory(document.querySelector("#isf-interactive-story"), storyData);
   });
-
 
 })(window);
